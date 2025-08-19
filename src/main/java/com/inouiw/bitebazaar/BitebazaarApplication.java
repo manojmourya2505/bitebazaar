79018f8b6487d461d20d005554fbacd453fb3a8d
Manoj Mourya Neeli <neelimanoj.mourya@ust.com>
2025-08-19 19:05:44 +0530

    chore: bootstrap bitebazaar project

---
+++ src/main/java/com/inouiw/bitebazaar/BitebazaarApplication.java
@@ -0,0 +1,13 @@
+package com.inouiw.bitebazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitebazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitebazaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inouiw/bitebazaar/BitebazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inouiw.bitebazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitebazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
